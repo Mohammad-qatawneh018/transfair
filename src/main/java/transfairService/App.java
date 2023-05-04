50b5ff78175c46e32888d56a2d814a685c093b9b
rahaf qatawneh <moham@DESKTOP-AO412H5>
2023-05-04 15:15:26 +0300

    fix the main app

--- src/main/java/transfairService/App.java
+++ src/main/java/transfairService/App.java
@@ -14,22 +14,35 @@ public class App {
 		Transfairs transfair = new Transfairs();
 
 		TransfairDataAccess dataAccess = new TransfairDataAccess();
+
+		// setting account
+		/*
+		 * account.setAccountName("Mohammad"); account.setAccountNumber(123);
+		 * account.setBalance(100);
+		 * 
+		 * account1.setAccountName("khalid"); account1.setAccountNumber(456);
+		 * account1.setBalance(188);
+		 * 
+		 * account2.setAccountName("ali"); account2.setAccountNumber(789);
+		 * account2.setBalance(50);
+		 * 
+		 * //adding accounts to database dataAccess.addAccount(account);
+		 * dataAccess.addAccount(account1); dataAccess.addAccount(account2);
+		 * 
+		 */
+
 		transfair.setAmount(174);
 		transfair.setToAccountId(3);
 
 		dataAccess.addTransfair(transfair);
 
-		 //dataAccess.addTransfair(transfair);
+		// dataAccess.addTransfair(transfair);
 
-		
 		System.out.println("accounts");
 		for (Accounts acc : dataAccess.getAccounts()) {
 			JK.print(acc);
 
 		}
-		
-		 
-
 
 		System.out.println("transfairs");
 		for (Transfairs acc : dataAccess.getTransfairs()) {
